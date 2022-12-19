c54305b93344a9495df77a1725ccbe28deb58854
Brenda <91943541+BrendaCredera@users.noreply.github.com>
2022-12-19 17:15:44 +0000

    basic components

--- CV-Frontend/cv-frontend/src/App.js
+++ CV-Frontend/cv-frontend/src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Welcome</h1>
+      <h2>This is my world</h2>
+      <div class="grid-container">
+        <div>Details</div>
+        <div>Games</div>
+        <div>Origin Story</div>
+        <div>FAQ</div>
+      </div>
     </div>
   );
 }
